c71d61e4764c89ec05380fb5d608f072f2c6480b
Kobe42io <thomas.david.dev@gmail.com>
2024-07-10 12:25:53 +0200

    okok

--- poke.js
+++ poke.js
@@ -1,24 +1,53 @@
-const pokemon = [
+// const pokemon = [
+//   {
+//     name: "Salameche",
+//     type: "feu",
+//     pv: 60,
+//     attaque(pokemon) {
+//       return console.log(
+//         `${this.name} attaque ${
+//           pokemon.name
+//         } et lui enleve ${attAleatoir} PV :::::: ${pokemon.name} n'a plus que ${
+//           pokemon.pv - attAleatoir
+//         } PV`
+//       );
+//     },
+//   },
 
-    {name: "Salameche", 
-        type: "feu", 
-        pv: 60,
-        avatar: 'img',
-    },
+//   { name: "Carapuce", type: "eau", pv: 50 },
+//   { name: "bulbizar", type: "plante", pv: 50 },
+// ];
+//pokemon[0].attaque(pokemon[1]);
+//console.log(pokemon[0].name);
 
-    {name: "Carapuce", type: "eau", pv: 50},
-    {name: "bulbizar", type: "plante", pv: 50},
-    
-]
+class Pokemon {
+  constructor(n, t, p) {
+    this.name = n;
+    this.type = t;
+    this.pv = p;
+  }
 
-let attAleatoir = Math.floor(Math.random()*10) +1
-
-function attaque(pokemon){
-    return console.log(` attaque  et lui enleve ${pv - pp}`);
+  attaque(pokemon) {
+    return console.log(
+      `${this.name} attaque ${
+        pokemon.name
+      } et lui enleve ${attAleatoir} PV :::::: ${pokemon.name} n'a plus que ${
+        pokemon.pv - attAleatoir
+      } PV`
+    );
+  }
 }
 
+//réutiliser le constructor parent
+class bulbizar extends Pokemon {
+  constructor(n, t, p, c) {
+    super(n, t, p);
+    this.color = c;
+  }
+}
 
+const bulbizar = new Pokemon("bulbizar", "plante", 50);
 
+let attAleatoir = Math.floor(Math.random() * 10) + 1;
 
-
-console.log(pokemon[0].name)
+console.log("classe pokemon ====>", bulbizar.name);
--- test.js
+++ test.js
@@ -1,36 +1,34 @@
-let x = "hello world"
-let y = 69
-let z = 1-1
-let even = 10
-let p = 'kobe '
-let p1 = "bryant "
+let x = "hello world";
+let y = 69;
+let z = 1 - 1;
+let even = 10;
+let p = "kobe ";
+let p1 = "bryant ";
 var arr = [];
-p1 += '24'
+p1 += "24";
 
-let dice = Math.floor(Math.random()*6) +1
+let dice = Math.floor(Math.random() * 6) + 1;
 
+let d = new Date();
+let toto = new Date("1989-06-13");
+let age = Math.floor((d - toto) / (365 * 24 * 3600000));
 
+let phrase = "my favorite dessert is jello";
+console.log(`pour l'exercice la reponse est: `, phrase.indexOf("j"));
 
+let phraseWithSlice = phrase.slice(22, 24);
+console.log(phraseWithSlice);
 
-let phrase = "my favorite dessert is jello"
-console.log( `pour l'exercice la reponse est: `, phrase.indexOf('j'))
+console.log(x);
 
-let phraseWithSlice = phrase.slice(22,24)
-console.log(phraseWithSlice)
+console.log(`hello ${p + p1} \n UP \n DOWN`);
 
+arr.push(x, y, z);
 
-console.log(x)
-
-console.log(`hello ${p+p1} \n UP \n DOWN`)
-
-
-
-arr.push(x, y, z)
-
-if(even %2 == 0){
-    console.log(arr)
+if (even % 2 == 0) {
+  console.log(arr);
 }
-    
+
 /*const reponse = prompt("quelle est ton age")
 
 if (reponse > 18){
@@ -38,10 +36,10 @@ if (reponse > 18){
 }
 */
 
-for (let i = 1;i < 12; i++){
-    if(i%2 != 0){
-        console.log(`i est égale à ${i}`)
-    }
+for (let i = 1; i < 12; i++) {
+  if (i % 2 != 0) {
+    console.log(`i est égale à ${i}`);
+  }
 }
 /*
 for (let i = 1;i < 12; i+=2){
@@ -50,33 +48,34 @@ for (let i = 1;i < 12; i+=2){
 
 }*/
 
-
 /*
 OBJECT
 */
 
 let objP1 = {
-    prenom: "kobe",
-    nom : "bryant",
-    number: 24,
-    post: "Forward",
-    "nickname": "blackMamba",
-    func: function hello(){
-        console.log("je suis la function de l'objet p1 (et je passe dans la condition if")
-    }
+  prenom: "kobe",
+  nom: "bryant",
+  number: 24,
+  post: "Forward",
+  nickname: "blackMamba",
+  func: function hello() {
+    console.log(
+      "je suis la function de l'objet p1 (et je passe dans la condition if"
+    );
+  },
+};
+
+objP1.country = "USA";
+
+console.log("valeur de l'objet avec la valeur de .prénom==>", objP1.prenom);
+console.log("valeur de l'objet ==>", objP1);
+if ("nom" in objP1) {
+  objP1.func();
 }
 
-objP1.country = "USA"
+const tab = Object.keys(objP1);
+const tab2 = Object.values(objP1);
 
-console.log("valeur de l'objet avec la valeur de .prénom==>", objP1.prenom)
-console.log("valeur de l'objet ==>", objP1)
-if("nom" in objP1){
-    objP1.func()
-}
-
-const tab = Object.keys(objP1)
-const tab2 = Object.values(objP1)
-
-for(let i=0; i<tab.length -2;i++){
-    console.log(`boucle for => ${tab[i]} : ${tab2[i]}`)
+for (let i = 0; i < tab.length - 2; i++) {
+  console.log(`boucle for => ${tab[i]} : ${tab2[i]}`);
 }
